6d9a500f11d235f39deed392a5bb602ae1fdfdab
Xinrea <s11s@vip.qq.com>
2017-09-25 14:13:33 +0800

    use nana as gui lib

--- SimpleChat.cpp
+++ SimpleChat.cpp
@@ -2,68 +2,25 @@
 //
 
 #include "stdafx.h"
-#include "SimpleChat.h"
 #include "communicate.h"
-#include "resource.h"
-#pragma comment(linker,"\"/manifestdependency:type='win32' name = 'Microsoft.Windows.Common-Controls' version = '6.0.0.0' \
-processorArchitecture = '*' publicKeyToken = '6595b64144ccf1df' language = '*'\"")
-
-INT_PTR CALLBACK DlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam);
-
-HINSTANCE hgInst;
-int WINAPI WinMain(HINSTANCE hInstace, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    hgInst = hInstace;
-    HWND hdlg = CreateDialog(hInstace, MAKEINTRESOURCE(IDD_MAIN), GetDesktopWindow(),(DLGPROC)DlgProc);//显示对话框
-    if (!hdlg)
-    {
-	return 0;
-    }
-    ShowWindow(hdlg, SW_SHOW);
-    MSG msg;
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-	TranslateMessage(&msg);
-	DispatchMessage(&msg);
-    }
-    return 0;
-}
-
-INT_PTR CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+#include <gui.hpp>
+#include <gui/widgets/label.hpp>
+int main()
 {
-    switch (message)
-    {
-    case WM_INITDIALOG:
-	    {
-		// 设置对话框的图标     
-		SendMessage(hDlg, WM_SETICON, ICON_SMALL, (LPARAM)LoadIcon(hgInst, MAKEINTRESOURCE(IDI_SIMPLECHAT)));
-		break;
-	    }
-    case WM_COMMAND:
-	    {
-		int wmId = LOWORD(wParam);
-		switch (wmId) {
-			case IDOK:
-				{
-				communicate sign_in(L"222.20.105.51", 12000);//test
-				sign_in.sconnect();
-				sign_in.sendmsg("123");
-				break;
-				}
-		}
-	    }
-	
-    case WM_SYSCOMMAND:
-	    {
-		if (wParam == SC_CLOSE)
-		{
-			PostQuitMessage(0);//退出     
-		}
-		break;
-	    }
-	
-    default:
-	return DefWindowProc(hDlg, message, wParam, lParam);
-    }
-
-    return (INT_PTR)FALSE;
+	//All names of Nana is in the namespace nana;
+	using namespace nana;
+	//Define a form object, class form will create a window
+	//when a form instance is created.
+	//The new window default visibility is false.
+	form fm;
+	//Define a label on the fm(form) with a specified area,
+	//and set the caption.
+	label lb{ fm, rectangle{ 10, 10, 100, 100 } };
+	lb.caption("Hello, world!");
+	//Expose the form.
+	fm.show();
+	//Pass the control of the application to Nana's event
+	//service. It blocks the execution for dispatching user
+	//input until the form is closed.
+	exec();
 }
--- stdafx.h
+++ stdafx.h
@@ -7,13 +7,7 @@
 #ifndef UNICODE
 #define UNICODE
 #endif
-#include "targetver.h"
 
-#define WIN32_LEAN_AND_MEAN             // 从 Windows 头中排除极少使用的资料
-// Windows 头文件: 
-#include <windows.h>
-
-// C 运行时头文件
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
@@ -23,5 +17,3 @@
 #include <ws2tcpip.h>
 #include <stdio.h>
 
-
-// TODO:  在此处引用程序需要的其他头文件
